2968bb133256124eced277d4300a2cc5a2d76264
shahar <yarn@ycyclop.localdomain>
2020-03-23 18:36:51 +0200

    Finished adding dns and http tests and formatted the code

--- tests/fixtures/marine/marine_fixtures.py
+++ tests/fixtures/marine/marine_fixtures.py
@@ -1,15 +1,24 @@
 import os
 from ipaddress import IPv4Address
 from random import getrandbits, randint, choices
+from typing import TypedDict, List
 import string
 
 import pytest
 from pypacker.layer12 import ethernet, arp
 from pypacker.layer3 import ip, icmp
 from pypacker.layer4 import tcp, udp
-from pypacker.layer567 import dns
+from pypacker.layer567 import dns, http
 
 from marine import Marine
+
+
+class HttpDescription(TypedDict):
+    http_type: str
+    uri: str
+    version: str
+    host: str
+    body: str
 
 
 def create_random_ip() -> str:
@@ -17,155 +26,241 @@ def create_random_ip() -> str:
 
 
 def create_random_mac() -> str:
-    return '00:00:00:%02x:%02x:%02x' % (randint(0, 255), randint(0, 255), randint(0, 255))
+    return "00:00:00:%02x:%02x:%02x" % (
+        randint(0, 255),
+        randint(0, 255),
+        randint(0, 255),
+    )
 
 
-def create_random_url(length: int) -> str:
-    return 'www.%s.com' % (''.join(choices(string.ascii_lowercase, k=length)))
+def create_random_string(length: int) -> str:
+    return "".join(choices(string.ascii_lowercase, k=length))
+
+
+def create_random_url(length: int = 8) -> str:
+    return "www.%s.com" % (create_random_string(length))
+
+
+def create_random_http_get() -> HttpDescription:
+    return {
+        "http_type": "GET",
+        "uri": f"/{create_random_string(8)}",
+        "version": "HTTP/1.1",
+        "host": create_random_url(),
+        "body": create_random_string(20),
+    }
 
 
 @pytest.fixture
-def url_1():
-    return create_random_url(8)
+def http_get() -> HttpDescription:
+    return create_random_http_get()
 
 
 @pytest.fixture
-def ip_1():
+def url_1() -> str:
+    return create_random_url()
+
+
+@pytest.fixture
+def ip_1() -> str:
     return create_random_ip()
 
 
 @pytest.fixture
-def ip_2():
+def ip_2() -> str:
     return create_random_ip()
 
 
 @pytest.fixture
-def mac_1():
+def mac_1() -> str:
     return create_random_mac()
 
 
 @pytest.fixture
-def mac_2():
+def mac_2() -> str:
     return create_random_mac()
 
 
 @pytest.fixture
-def broadcast_mac():
-    return 'ff:ff:ff:ff:ff:ff'
+def broadcast_mac() -> str:
+    return "ff:ff:ff:ff:ff:ff"
 
 
 @pytest.fixture
-def port_1():
+def port_1() -> int:
     return randint(1, 60000)
 
 
 @pytest.fixture
-def port_2():
+def port_2() -> int:
     return randint(1, 60000)
 
 
 @pytest.fixture
-def port_3():
+def port_3() -> int:
     return randint(1, 60000)
 
 
 @pytest.fixture
-def port_4():
+def port_4() -> int:
     return randint(1, 60000)
 
 
 @pytest.fixture
-def icmp_type():
+def dns_port() -> int:
+    return 53
+
+
+@pytest.fixture
+def http_port() -> int:
+    return 80
+
+
+@pytest.fixture
+def icmp_echo_type() -> int:
     return 8
 
 
 @pytest.fixture
-def byte_field_1():
-    return randint(1, 255)
+def extracted_fields_from_ethernet_packet() -> List[str]:
+    return ["eth.src", "eth.dst"]
 
 
 @pytest.fixture
-def byte_field_2():
-    return 1
+def extracted_fields_from_arp_packet(
+    extracted_fields_from_ethernet_packet,
+) -> List[str]:
+    return extracted_fields_from_ethernet_packet + [
+        "arp.src.hw_mac",
+        "arp.src.proto_ipv4",
+        "arp.dst.hw_mac",
+        "arp.dst.proto_ipv4",
+    ]
 
 
 @pytest.fixture
-def extracted_fields_from_ethernet_packet():
-    return ['eth.src', 'eth.dst']
+def extracted_fields_from_ip_packet(extracted_fields_from_ethernet_packet) -> List[str]:
+    return extracted_fields_from_ethernet_packet + ["ip.src", "ip.dst"]
 
 
 @pytest.fixture
-def extracted_fields_from_arp_packet(extracted_fields_from_ethernet_packet):
-    return extracted_fields_from_ethernet_packet + ['arp.src.hw_mac', 'arp.src.proto_ipv4', 'arp.dst.hw_mac', 'arp.dst.proto_ipv4']
+def extracted_fields_from_icmp_packet(extracted_fields_from_ip_packet) -> List[str]:
+    return extracted_fields_from_ip_packet + ["icmp.type"]
 
 
 @pytest.fixture
-def extracted_fields_from_ip_packet(extracted_fields_from_ethernet_packet):
-    return extracted_fields_from_ethernet_packet + ['ip.src', 'ip.dst']
+def extracted_fields_from_tcp_packet(extracted_fields_from_ip_packet) -> List[str]:
+    return extracted_fields_from_ip_packet + ["tcp.srcport", "tcp.dstport"]
 
 
 @pytest.fixture
-def extracted_fields_from_icmp_packet(extracted_fields_from_ip_packet):
-    return extracted_fields_from_ip_packet + ['icmp.type']
+def extracted_fields_from_udp_packet(extracted_fields_from_ip_packet) -> List[str]:
+    return extracted_fields_from_ip_packet + ["udp.srcport", "udp.dstport"]
 
 
 @pytest.fixture
-def extracted_fields_from_tcp_packet(extracted_fields_from_ip_packet):
-    return extracted_fields_from_ip_packet + ['tcp.srcport', 'tcp.dstport']
+def extracted_fields_from_dns_packet(extracted_fields_from_udp_packet) -> List[str]:
+    return extracted_fields_from_udp_packet + ["dns.qry.name"]
 
 
 @pytest.fixture
-def extracted_fields_from_udp_packet(extracted_fields_from_ip_packet):
-    return extracted_fields_from_ip_packet + ['udp.srcport', 'udp.dstport']
+def extracted_fields_from_http_packet(extracted_fields_from_tcp_packet) -> List[str]:
+    return extracted_fields_from_tcp_packet + [
+        "http.request.method",
+        "http.request.uri",
+        "http.request.version",
+        "http.host",
+    ]
 
 
 @pytest.fixture
-def extracted_fields_from_dns_packet(extracted_fields_from_udp_packet):
-    return extracted_fields_from_udp_packet + ['dns.id', 'dns.count.queries', 'dns.qry.name']
+def ethernet_packet(mac_1: str, mac_2: str) -> ethernet.Ethernet:
+    return ethernet.Ethernet(src_s=mac_1, dst_s=mac_2)
 
 
 @pytest.fixture
-def arp_packet(mac_1, broadcast_mac, ip_1, ip_2):
-    packet = (ethernet.Ethernet(src_s=mac_1, dst_s=broadcast_mac)
-              + arp.ARP(sha_s=mac_1, spa_s=ip_1, tha_s=broadcast_mac, tpa_s=ip_2))
+def arp_packet(mac_1: str, broadcast_mac: str, ip_1: str, ip_2: str) -> bytes:
+    packet = ethernet.Ethernet(src_s=mac_1, dst_s=broadcast_mac) + arp.ARP(
+        sha_s=mac_1, spa_s=ip_1, tha_s=broadcast_mac, tpa_s=ip_2
+    )
     return packet.bin()
 
 
 @pytest.fixture
-def icmp_packet(mac_1, mac_2, ip_1, ip_2, icmp_type):
-    packet = (ethernet.Ethernet(src_s=mac_1, dst_s=mac_2) + ip.IP(src_s=ip_1, dst_s=ip_2, p=ip.IP_PROTO_ICMP)
-              + icmp.ICMP(type=icmp_type)) + icmp.ICMP.Echo(id=1, ts=123456789, body_bytes=b'data')
+def icmp_packet(
+    ethernet_packet: ethernet.Ethernet, ip_1: str, ip_2: str, icmp_echo_type: int
+) -> bytes:
+    packet = (
+        ethernet_packet
+        + ip.IP(src_s=ip_1, dst_s=ip_2, p=ip.IP_PROTO_ICMP)
+        + icmp.ICMP(type=icmp_echo_type)
+        + icmp.ICMP.Echo()
+    )
     return packet.bin()
 
 
 @pytest.fixture
-def tcp_packet(mac_1, mac_2, ip_1, ip_2, port_1, port_2):
-    packet = (ethernet.Ethernet(src_s=mac_1, dst_s=mac_2) + ip.IP(src_s=ip_1, dst_s=ip_2)
-              + tcp.TCP(sport=port_1, dport=port_2))
+def tcp_packet(
+    ethernet_packet: ethernet.Ethernet, ip_1: str, ip_2: str, port_1: int, port_2: int
+) -> bytes:
+    packet = (
+        ethernet_packet
+        + ip.IP(src_s=ip_1, dst_s=ip_2)
+        + tcp.TCP(sport=port_1, dport=port_2)
+    )
     return packet.bin()
 
 
 @pytest.fixture
-def pypacker_udp_packet(mac_1, mac_2, ip_1, ip_2, port_3, port_4):
-    packet = (ethernet.Ethernet(src_s=mac_1, dst_s=mac_2) + ip.IP(src_s=ip_1, dst_s=ip_2, p=ip.IP_PROTO_UDP)
-              + udp.UDP(sport=port_3, dport=53))
-    return packet
-
-
-@pytest.fixture
-def udp_packet(pypacker_udp_packet):
-    return pypacker_udp_packet.bin()
-
-
-@pytest.fixture
-def dns_packet(pypacker_udp_packet, byte_field_1, byte_field_2, url_1):
-    packet = pypacker_udp_packet + dns.DNS(id=byte_field_1, questions_count=byte_field_2, flags=0x0100,
-              queries=[dns.DNS.Query(name_s=url_1, type=1, cls=1)])
-    print(url_1)
+def udp_packet(
+    ethernet_packet: ethernet.Ethernet, ip_1: str, ip_2: str, port_3: int, port_4: int
+) -> bytes:
+    packet = (
+        ethernet_packet
+        + ip.IP(src_s=ip_1, dst_s=ip_2, p=ip.IP_PROTO_UDP)
+        + udp.UDP(sport=port_3, dport=port_4)
+    )
     return packet.bin()
 
 
-@pytest.fixture(scope='session')
-def marine_instance():
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libmarine.so')
+@pytest.fixture
+def dns_packet(
+    ethernet_packet: ethernet.Ethernet,
+    ip_1: str,
+    ip_2: str,
+    port_3: int,
+    dns_port: int,
+    url_1: str,
+) -> bytes:
+    packet = (
+        ethernet_packet
+        + ip.IP(src_s=ip_1, dst_s=ip_2, p=ip.IP_PROTO_UDP)
+        + udp.UDP(sport=port_3, dport=dns_port)
+    ) + dns.DNS(queries=[dns.DNS.Query(name_s=url_1, type=1, cls=1)])
+    return packet.bin()
+
+
+@pytest.fixture
+def http_packet(
+    ethernet_packet: ethernet.Ethernet,
+    ip_1: str,
+    ip_2: str,
+    port_1: int,
+    http_port: int,
+    http_get: HttpDescription,
+) -> bytes:
+    packet = (
+        ethernet_packet
+        + ip.IP(src_s=ip_1, dst_s=ip_2)
+        + tcp.TCP(sport=port_1, dport=http_port)
+        + http.HTTP(
+            f'{http_get["http_type"]} {http_get["uri"]} {http_get["version"]}\r\nHost: {http_get["host"]}\r\n\r\n{http_get["body"]}\r\n'.encode()
+        )
+    )
+    return packet.bin()
+
+
+@pytest.fixture(scope="session")
+def marine_instance() -> Marine:
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "libmarine.so")
     return Marine(path)
--- tests/marine/test_marine.py
+++ tests/marine/test_marine.py
@@ -2,115 +2,252 @@
 Note: in order to run the tests, you must put libmarine.so next to the marine_fixtures.py file
 """
 import pytest
+from typing import List, Dict
+from marine import Marine
 
 
-# TODO: test more common protocols: ARP, ICMP, HTTP, etc.
-
-
-def test_arp_filter_and_parse(marine_instance, arp_packet, mac_1, broadcast_mac, ip_1, ip_2,
-                              extracted_fields_from_arp_packet):
-    expected = dict(zip(extracted_fields_from_arp_packet, map(str, [mac_1, broadcast_mac, mac_1, ip_1, broadcast_mac, ip_2])))
-    passed, output = marine_instance.filter_and_parse(arp_packet, 'arp', 'arp', extracted_fields_from_arp_packet)
-
-    assert passed
-    assert expected == output
-
-
-def test_icmp_filter_and_parse(marine_instance, icmp_packet, mac_1, mac_2, ip_1, ip_2, icmp_type,
-                               extracted_fields_from_icmp_packet):
-    expected = dict(zip(extracted_fields_from_icmp_packet, map(str, [mac_1, mac_2, ip_1, ip_2, icmp_type])))
-
-    passed, output = marine_instance.filter_and_parse(icmp_packet, 'ip', 'icmp', extracted_fields_from_icmp_packet)
-
-    assert passed
-    assert expected == output
-
-
-def test_tcp_filter_and_parse(marine_instance, tcp_packet, mac_1, mac_2, ip_1, ip_2, port_1, port_2,
-                              extracted_fields_from_tcp_packet):
-    expected = dict(zip(extracted_fields_from_tcp_packet, map(str, [mac_1, mac_2, ip_1, ip_2, port_1, port_2])))
-
-    passed, output = marine_instance.filter_and_parse(tcp_packet, 'ip', 'tcp', extracted_fields_from_tcp_packet)
+def test_arp_filter_and_parse(
+    marine_instance: Marine,
+    arp_packet: bytes,
+    mac_1: str,
+    broadcast_mac: str,
+    ip_1: str,
+    ip_2: str,
+    extracted_fields_from_arp_packet: List[str],
+):
+    """
+    Testing layer 2 protocol parsing.
+    """
+    expected = dict(
+        zip(
+            extracted_fields_from_arp_packet,
+            map(str, [mac_1, broadcast_mac, mac_1, ip_1, broadcast_mac, ip_2]),
+        )
+    )
+    passed, output = marine_instance.filter_and_parse(
+        arp_packet, "arp", "arp", extracted_fields_from_arp_packet
+    )
 
     assert passed
     assert expected == output
 
 
-def test_udp_filter_and_parse(marine_instance, udp_packet, mac_1, mac_2, ip_1, ip_2, port_3, port_4,
-                              extracted_fields_from_udp_packet):
-    expected = dict(zip(extracted_fields_from_udp_packet, map(str, [mac_1, mac_2, ip_1, ip_2, port_3, port_4])))
-    passed, output = marine_instance.filter_and_parse(udp_packet, 'ip', 'udp', extracted_fields_from_udp_packet)
+def test_icmp_filter_and_parse(
+    marine_instance: Marine,
+    icmp_packet: bytes,
+    mac_1: str,
+    mac_2: str,
+    ip_1: str,
+    ip_2: str,
+    icmp_echo_type: int,
+    extracted_fields_from_icmp_packet: List[str],
+):
+    """
+    Testing layer 3 protocol parsing.
+    """
+    expected = dict(
+        zip(
+            extracted_fields_from_icmp_packet,
+            map(str, [mac_1, mac_2, ip_1, ip_2, icmp_echo_type]),
+        )
+    )
+
+    passed, output = marine_instance.filter_and_parse(
+        icmp_packet, "ip", "icmp", extracted_fields_from_icmp_packet
+    )
 
     assert passed
     assert expected == output
 
 
-def test_dns_filter_and_parse(marine_instance, dns_packet, mac_1, mac_2, ip_1, ip_2, port_3, port_4,
-                              byte_field_1, byte_field_2, url_1, extracted_fields_from_dns_packet):
-    expected = dict(zip(extracted_fields_from_dns_packet, map(str, 
-                [mac_1, mac_2, ip_1, ip_2, port_3, port_4, byte_field_1, byte_field_2, url_1])))
-    passed, output = marine_instance.filter_and_parse(dns_packet, 'ip', 'dns', extracted_fields_from_dns_packet)
+def test_tcp_filter_and_parse(
+    marine_instance: Marine,
+    tcp_packet: bytes,
+    mac_1: str,
+    mac_2: str,
+    ip_1: str,
+    ip_2: str,
+    port_1: int,
+    port_2: int,
+    extracted_fields_from_tcp_packet: List[str],
+):
+    """
+    Testing layer 4 protocol parsing.
+    """
+    expected = dict(
+        zip(
+            extracted_fields_from_tcp_packet,
+            map(str, [mac_1, mac_2, ip_1, ip_2, port_1, port_2]),
+        )
+    )
+
+    passed, output = marine_instance.filter_and_parse(
+        tcp_packet, "ip", "tcp", extracted_fields_from_tcp_packet
+    )
 
     assert passed
     assert expected == output
 
 
+def test_dns_filter_and_parse(
+    marine_instance: Marine,
+    dns_packet: bytes,
+    mac_1: str,
+    mac_2: str,
+    ip_1: str,
+    ip_2: str,
+    port_3: int,
+    dns_port: int,
+    url_1: str,
+    extracted_fields_from_dns_packet: List[str],
+):
+    """
+    Testing a layer 567 protocol, and dns specificaly to check query parsing.
+    """
+    expected = dict(
+        zip(
+            extracted_fields_from_dns_packet,
+            map(str, [mac_1, mac_2, ip_1, ip_2, port_3, dns_port, url_1]),
+        )
+    )
+    passed, output = marine_instance.filter_and_parse(
+        dns_packet, "ip", "dns", extracted_fields_from_dns_packet
+    )
 
-def test_filter_and_parse_without_fields(marine_instance, tcp_packet):
-    passed, output = marine_instance.filter_and_parse(tcp_packet, 'ip', 'tcp')
+    assert passed
+    assert expected == output
+
+
+def test_http_filter_and_parse(
+    marine_instance: Marine,
+    http_packet: bytes,
+    mac_1: str,
+    mac_2: str,
+    ip_1: str,
+    ip_2: str,
+    port_1: int,
+    http_port: int,
+    http_get: Dict[str, str],
+    extracted_fields_from_http_packet: List[str],
+):
+    """
+    Testing a layer 567 protocol, and http speificaly for text fields.
+    """
+    expected = dict(
+        zip(
+            extracted_fields_from_http_packet,
+            map(
+                str,
+                [
+                    mac_1,
+                    mac_2,
+                    ip_1,
+                    ip_2,
+                    port_1,
+                    http_port,
+                    http_get["http_type"],
+                    http_get["uri"],
+                    http_get["version"],
+                    http_get["host"],
+                ],
+            ),
+        )
+    )
+    passed, output = marine_instance.filter_and_parse(
+        http_packet, "ip", "http", extracted_fields_from_http_packet
+    )
+
+    assert passed
+    assert expected == output
+
+
+def test_filter_and_parse_without_fields(marine_instance: Marine, tcp_packet: bytes):
+    passed, output = marine_instance.filter_and_parse(tcp_packet, "ip", "tcp")
 
     assert passed
     assert output is None
 
 
-def test_filter_and_parse_without_filters(marine_instance, tcp_packet, mac_1, mac_2, ip_1, ip_2, port_1, port_2,
-                                          extracted_fields_from_tcp_packet):
-    expected = dict(zip(extracted_fields_from_tcp_packet, map(str, [mac_1, mac_2, ip_1, ip_2, port_1, port_2])))
-    passed, output = marine_instance.filter_and_parse(tcp_packet, fields=extracted_fields_from_tcp_packet)
+def test_filter_and_parse_without_filters(
+    marine_instance: Marine,
+    tcp_packet: bytes,
+    mac_1: str,
+    mac_2: str,
+    ip_1: str,
+    ip_2: str,
+    port_1: int,
+    port_2: int,
+    extracted_fields_from_tcp_packet: List[str],
+):
+    expected = dict(
+        zip(
+            extracted_fields_from_tcp_packet,
+            map(str, [mac_1, mac_2, ip_1, ip_2, port_1, port_2]),
+        )
+    )
+    passed, output = marine_instance.filter_and_parse(
+        tcp_packet, fields=extracted_fields_from_tcp_packet
+    )
 
     assert passed
     assert expected == output
 
 
-def test_packet_doesnt_pass_filter_because_of_bfp(marine_instance, tcp_packet, extracted_fields_from_tcp_packet):
-    passed, output = marine_instance.filter_and_parse(tcp_packet, 'arp', fields=extracted_fields_from_tcp_packet)
+def test_packet_doesnt_pass_filter_because_of_bfp(
+    marine_instance: Marine,
+    tcp_packet: bytes,
+    extracted_fields_from_tcp_packet: List[str],
+):
+    passed, output = marine_instance.filter_and_parse(
+        tcp_packet, "arp", fields=extracted_fields_from_tcp_packet
+    )
 
     assert not passed
     assert output is None
 
 
-def test_packet_doesnt_pass_filter_because_of_display_filter(marine_instance, tcp_packet,
-                                                             extracted_fields_from_tcp_packet):
-    passed, output = marine_instance.filter_and_parse(tcp_packet, display_filter='udp',
-                                                      fields=extracted_fields_from_tcp_packet)
+def test_packet_doesnt_pass_filter_because_of_display_filter(
+    marine_instance: Marine,
+    tcp_packet: bytes,
+    extracted_fields_from_tcp_packet: List[str],
+):
+    passed, output = marine_instance.filter_and_parse(
+        tcp_packet, display_filter="udp", fields=extracted_fields_from_tcp_packet
+    )
 
     assert not passed
     assert output is None
 
 
-def test_illegal_bpf_in_filter_and_parse(marine_instance, tcp_packet):
+def test_illegal_bpf_in_filter_and_parse(marine_instance: Marine, tcp_packet: bytes):
     with pytest.raises(ValueError) as excinfo:
-        marine_instance.filter_and_parse(tcp_packet, bpf='what is this bpf?')
-    assert 'Failed compiling the BPF' in str(excinfo)
+        marine_instance.filter_and_parse(tcp_packet, bpf="what is this bpf?")
+    assert "Failed compiling the BPF" in str(excinfo)
 
 
-def test_illegal_display_filter_in_filter_and_parse(marine_instance, tcp_packet):
+def test_illegal_display_filter_in_filter_and_parse(
+    marine_instance: Marine, tcp_packet: bytes
+):
     with pytest.raises(ValueError) as excinfo:
-        marine_instance.filter_and_parse(tcp_packet, display_filter='illegal_filter')
-    assert 'neither a field nor a protocol name' in str(excinfo)
+        marine_instance.filter_and_parse(tcp_packet, display_filter="illegal_filter")
+    assert "neither a field nor a protocol name" in str(excinfo)
 
 
-def test_illegal_fields_in_filter_and_parse(marine_instance, tcp_packet):
+def test_illegal_fields_in_filter_and_parse(marine_instance: Marine, tcp_packet: bytes):
     with pytest.raises(ValueError) as excinfo:
-        marine_instance.filter_and_parse(tcp_packet, fields=['illegal_field_1', 'illegal_field_2', 'ip.src'])
+        marine_instance.filter_and_parse(
+            tcp_packet, fields=["illegal_field_1", "illegal_field_2", "ip.src"]
+        )
     err_msg = str(excinfo)
 
-    assert 'illegal_field_1' in err_msg
-    assert 'illegal_field_2' in err_msg
-    assert 'ip.src' not in err_msg
+    assert "illegal_field_1" in err_msg
+    assert "illegal_field_2" in err_msg
+    assert "ip.src" not in err_msg
 
 
-def test_filter_and_parse_with_no_parameters(marine_instance, tcp_packet):
+def test_filter_and_parse_with_no_parameters(
+    marine_instance: Marine, tcp_packet: bytes
+):
     with pytest.raises(ValueError) as excinfo:
         marine_instance.filter_and_parse(tcp_packet)
-    assert 'must be passed' in str(excinfo)
+    assert "must be passed" in str(excinfo)
