b2be89afae168566948d3b38b4ad35cc1000fc53
Yehonatan Zecharia <yonti95@gmail.com>
2021-01-30 21:41:16 +0200

    Add typing to modify_auditwheel_policy script

--- scripts/modify_auditwheel_policy.py
+++ scripts/modify_auditwheel_policy.py
@@ -1,9 +1,17 @@
 import json
+from typing import List, TypedDict
+
+
+class Policy(TypedDict):
+    name: str
+    lib_whitelist: List[str]
+    # There are more fields bu they don't concern us
+
 
 policy_path = "/auditwheel/policy/policy.json"
 with open(policy_path) as f:
-    policies = json.load(f)
-manylinux2014 = [p for p in policies if p["name"] == "manylinux2014"][0]
+    policies: List[Policy] = json.load(f)
+[manylinux2014] = [p for p in policies if p["name"] == "manylinux2014"]
 manylinux2014["lib_whitelist"].append("libpcap.so.1")
 with open(policy_path, "w") as f:
     json.dump(policies, f, indent=2)
