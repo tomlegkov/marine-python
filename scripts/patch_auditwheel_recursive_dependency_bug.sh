8bdae333038cdf3c0b1e602258c2f8c88caf0a16
Yehonatan Zecharia <yonti95@gmail.com>
2021-01-30 21:29:31 +0200

    Change auditwheel patch to use patch instead of sed

--- scripts/patch_auditwheel_recursive_dependency_bug.sh
+++ scripts/patch_auditwheel_recursive_dependency_bug.sh
@@ -3,4 +3,4 @@
 # This is a patch to handle auditwheel's bug with recursive depenencides:
 # https://github.com/pypa/auditwheel/issues/48
 
-sed -i 's/if is_py_ext:/if True:/g' /auditwheel/wheel_abi.py
+patch -i "$(dirname ${BASH_SOURCE[0]})/wheel_abi.patch" /auditwheel/wheel_abi.py
