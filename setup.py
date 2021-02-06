3a5fb4d2b0f25461f0348fd76e5efa08732e871c
Tom Legkov <tom.legkov@outlook.com>
2021-02-06 21:39:28 +0200

    Bump version to 3.0.0
    Bump version to 3.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="marine",
-    version="0.1",
+    version="3.0.0",
     description="Python client for Marine",
     url="https://github.com/tomlegkov/marine-python",
     author="Tom Legkov",
