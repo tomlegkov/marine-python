dbb694c263643cf672507f6fe881dfc379a79af9
Tom Legkov <tom.legkov@outlook.com>
2024-09-27 17:19:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name="marine",
     version="3.1.2",
     description="Python client for Marine",
-    url="https://github.com/tomlegkov/marine-python",
-    author="Tom Legkov",
-    author_email="tom.legkov@outlook.com",
     packages=["marine"],
     include_package_data=True,
     package_data={
